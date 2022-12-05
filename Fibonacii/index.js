f7e8c5878b49ceb8c8ff92106d0b7eb374138192
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-05 13:30:28 +0530

    fibonaccii-series-by-iteration-method

--- Fibonacii/index.js
+++ Fibonacii/index.js
@@ -1,17 +1,17 @@
 
 
 function FibonaciiSeries(n){
-    let s=0;
-    let n1=0;
-    let n2=1;
+  let n1=0;
+  let n2=1;
+  let s=0;
   for(let i=1; i<n; i++){
-     s=n1+n2;
-     n1=n2;
-     n2=s;
+    s+=n1+n2;
+    n1=n2;
+    n2=s;
   }
-  console.log(n2);
+  console.log(s);
 }
 
 
-let n=3;
+let n=7;
 FibonaciiSeries(n)
