f5f3ec7f36f6e8705e98a9ec13ad81e061bc6c70
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-07 10:18:38 +0530

    armstrong-number-checked

--- ArmStrong/index.js
+++ ArmStrong/index.js
@@ -1,13 +1,13 @@
   
    
       function ArmStrong(n){
-        n= n.toString();
-        let len= n.length;
+        n=n.toString();
+        let len=n.length;
         let out=0;
         for(let i of n){
-          out+= parseInt(i)**len
+          out+= i**len;
         }
-        out===parseInt(n)?console.log('ArmStrong Number'):console.log('Not an armStrong Number');
+        out===parseInt(n)?console.log('IT IS AN ARMSTRONG NUMBER'):console.log('NOT AN ARMSTRONG NUMBER');
       }
       let n=153;
       ArmStrong(n)
