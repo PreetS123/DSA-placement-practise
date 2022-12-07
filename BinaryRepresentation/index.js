83ae9d3f909bc24d0a168a4123dcaab917e87b19
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-07 10:33:04 +0530

    binary-representation-of-a-number

--- BinaryRepresentation/index.js
+++ BinaryRepresentation/index.js
@@ -1,12 +1,12 @@
 
 
    function BinaryRepresentation(n){
-           let bag=new Array(16).fill(0);
-       while(n>0){
-            bag.push(n%2);
-            n=Math.floor(n/2);  
-       }
-       console.log(bag.join(""))
+        let bag="";
+        while(n>0){
+            bag+= n%2;
+            n=Math.floor(n/2)
+        }
+        console.log(bag)
    }
 
 
