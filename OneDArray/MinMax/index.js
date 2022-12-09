c3f06473cc4b36d1f41168c5268e12ecbb93f09c
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-09 07:18:09 +0530

    finded-minimum-and-maximum-number-in-an-array

---
+++ OneDArray/MinMax/index.js
@@ -0,0 +1,20 @@
+
+function MinMax(arr,n){
+    let min=1/0;
+    let max=-1/0;
+    for(let i=0; i<n; i++){
+        if(arr[i]<min){
+            min=arr[i]
+        }
+        if(arr[i]>max){
+            max=arr[i]
+        }
+    }
+    console.log(min,max);
+}
+
+
+
+let arr=[3, 2, 1, 56, 10000, 167,-22];
+let n=arr.length;
+ MinMax(arr,n)
