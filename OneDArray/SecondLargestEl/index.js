5550a99b165390f35c76536b5e6700084e66d129
PreetS123 <preetbhardwaj517@gmail.com>
2022-12-12 07:16:04 +0530

    second-largest-element

--- OneDArray/SecondLargestEl/index.js
+++ OneDArray/SecondLargestEl/index.js
@@ -1,18 +1,18 @@
-/// find the second largest element from positive number array
+/// find the second largest element from positive number of array
 
 function SecondLagestEl(arr) {
-  let f = -1;
   let s = -1;
+  let f = -1;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > f) {
       s = f;
       f = arr[i];
-    } else if (arr[i] > s && arr[i] !== f) {
+    } else if (arr[i] > s && s !== f) {
       s = arr[i];
     }
   }
   console.log(s);
 }
 
-let arr = [2, 9, 7, 4];
+let arr = [2, 9, 7, 8, 4];
 SecondLagestEl(arr);
