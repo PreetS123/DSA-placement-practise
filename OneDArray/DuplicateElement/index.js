3881e36d338269fc78f70fc88f26e1b6ed9563e3
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-10 15:50:57 +0530

    dublicate element revision done

--- OneDArray/DuplicateElement/index.js
+++ OneDArray/DuplicateElement/index.js
@@ -1,22 +1,19 @@
 
 
 function RemoveDuplicate(n,arr){
-    let obj={};
-    for(let i=0; i<n; i++){
-        if(obj[arr[i]]==undefined){
-             obj[arr[i]]=1
+     let obj={};
+     for(let i=0; i<n; i++){
+        if(obj[arr[i]]===undefined){
+            obj[arr[i]]=1;
         }else{
             obj[arr[i]]++;
         }
-    }
-    // console.log(Object.keys(obj));
-    let c=0; 
-    let pad='';
-    for(let key in obj){
-       pad+=key+" " 
-       c++; 
-    }
-    console.log(pad,"\n",c)
+     }
+     let pad="";
+     for(var key in obj){
+       pad+=key+" ";
+     }
+     console.log(pad);
 }
 
 let arr=[1, 1, 3, 3,6,5,6]
