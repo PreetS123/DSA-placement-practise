cd3c4bf93bbc2826fba2f37be9e7ba6857cd6c32
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-10 16:08:33 +0530

    minimum maximum in an array done

--- OneDArray/MinMax/index.js
+++ OneDArray/MinMax/index.js
@@ -1,20 +1,20 @@
 
 function MinMax(arr,n){
-    let min=1/0;
-    let max=-1/0;
-    for(let i=0; i<n; i++){
-        if(arr[i]<min){
-            min=arr[i]
-        }
-        if(arr[i]>max){
-            max=arr[i]
-        }
+   let min=1/0;
+   let max=-1/0;
+   for(let i=0; i<n; i++){
+    if(arr[i]<min){
+        min=arr[i];
     }
-    console.log(min,max);
+    if(arr[i]>max){
+        max=arr[i];
+    }
+   }
+   console.log('Minimum:'+min," ",'Maximum:'+max);
 }
 
 
 
-let arr=[3, 2, 1, 56, 10000, 167,-22];
+let arr=[3, 2000, 1, 56, 10000, 167,-22];
 let n=arr.length;
  MinMax(arr,n)
