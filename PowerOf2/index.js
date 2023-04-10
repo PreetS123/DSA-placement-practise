710b38b55c628b040e5443e97dd34ff4e5b25559
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-10 17:04:14 +0530

    powe of two revised

--- PowerOf2/index.js
+++ PowerOf2/index.js
@@ -1,19 +1,17 @@
 
   function PowerOf2(n){
     let flag=false;
-    let c=0;
-   
-      while(n>1){
-        if(n%2===0){
-         n=n/2 
-         flag=true;
+     while (n>1){
+      if(n%2===0){
+        n=n/2;
+        flag=true;
       }else{
         flag=false;
         break;
       }
-    }
-    flag===true?console.log('yes'):console.log('no')
+     }
+     flag===true?console.log('Yes'):console.log('No');
   }
 
-  let n=66;
+  let n=128;
   PowerOf2(n);
