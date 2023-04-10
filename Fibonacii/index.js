ec027faed9b5ccf3447916065f7f7f0b13756f7c
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-10 13:07:39 +0530

    fibonacci series revision done

--- Fibonacii/index.js
+++ Fibonacii/index.js
@@ -1,15 +1,15 @@
 
 
 function FibonaciiSeries(n){
-  let zero=0;
-  let one=1;
-  let sum=0;
-  for(let i=1; i<n; i++){
-    sum=zero+one;
-    zero=one;
-    one=sum;
-  }
-  console.log(sum);
+ let zero=0;
+ let one=1;
+ let sum=0;
+ for(let i=1; i<n; i++){
+     sum=zero+one;
+     zero=one;
+     one=sum;
+ }
+ console.log(sum);
 }
 
 
