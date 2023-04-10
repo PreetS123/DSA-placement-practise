e8ce7a62418f3756c81e99033a8afa2aa8ab9e5a
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-10 16:00:49 +0530

    fizz buzz revision done

--- OneDArray/FizzBuzz/index.js
+++ OneDArray/FizzBuzz/index.js
@@ -1,17 +1,17 @@
 
 
 function FizzBuzz(n){
-   for(let i=1; i<=n; i++){
-         if(i%3===0 && i%5===0){
-            console.log('FizzBuzz');
-         }else if(i%5===0){
-            console.log('Buzz');
-         }else if(i%3===0){
-            console.log('Fizz')
-         }else{
-            console.log(i)
-         }
+  for(let i=1; i<=n; i++){
+   if(i%3===0 && i%5===0){
+      console.log('FizzBuzz');
+   } else if(i%3==0){
+        console.log('Fizz');
+   }else if(i%5==0){
+      console.log('Buzz');
+   }else{
+      console.log(i);
    }
+  }
 }
 
 let n=15;
