d6ac8eca8c24045714c44a3f4001ccdaa3aa8d8e
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-10 16:55:02 +0530

    second-largest revised

--- OneDArray/SecondLargestEl/index.js
+++ OneDArray/SecondLargestEl/index.js
@@ -3,16 +3,17 @@
 function SecondLagestEl(arr) {
   let s = -1;
   let f = -1;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > f) {
-      s = f;
-      f = arr[i];
-    } else if (arr[i] > s && s !== f) {
-      s = arr[i];
-    }
+ for(let i=0; i<arr.length; i++){
+  if(arr[i]>f){
+      s=f;
+      f=arr[i];
   }
+  // else if(s!==f && arr[i]>s){
+  //   s=arr[i];
+  // }
+ }
   console.log(s);
 }
 
-let arr = [2, 9, 7, 8, 4];
+let arr = [2,7,8,0,1,5,7,7,3];
 SecondLagestEl(arr);
