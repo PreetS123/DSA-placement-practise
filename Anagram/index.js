0b009329f5ecc0ea73ad1d8c886b788c9286bc47
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-10 11:36:31 +0530

    anagram question revised

--- Anagram/index.js
+++ Anagram/index.js
@@ -2,26 +2,26 @@
 
 function AnaGramChk(x,y,n,m){
     let flag=false;
-    if(n!==m){
-        flag=false;
-    }else{
-         x= x.split("").sort();
-         y=y.split("").sort();
-         for(let i=0; i<n; i++){
-            if(x[i]===y[i]){
-                flag=true;
-            }else{
-                flag=false;
-                break;
-            }
-         }   
-    }
-    flag===true?console.log('ANAGRAM'):console.log('NOT AN ANAGRAM');
+   if(n!==m){
+    flag=false;
+   }else{
+       x=x.split("").sort();
+       y=y.split("").sort();
+       for(let i=0; i<x.length; i++){
+          if(x[i]===y[i]){
+            flag=true;
+          }else{
+            fleg=false;
+            break;
+          }
+       }
+   }
+   flag===true?console.log('Yes'):console.log('No');
 }
 
 
 let x='abcd';
-let y='dabc';
+let y='ddbac';
 let n=x.length;
 let m=y.length;
 AnaGramChk(x,y,n,m);
