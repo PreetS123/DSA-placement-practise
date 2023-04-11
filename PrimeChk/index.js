2657104a7e8d15a94df51045d6a0e6bad8ea0f3e
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-11 12:34:47 +0530

    prime number revised

--- PrimeChk/index.js
+++ PrimeChk/index.js
@@ -2,12 +2,13 @@
   function PrimeCheck(n){
     let c=0;
     for(let i=2; i<n; i++){
-       if(n%i==0){
-              c++;
-       }
-    }
-    c>0?console.log('not a prime'):console.log('prime number')
+      if(n%i===0){
+        c++;
+      }  
+    }  
+    console.log('c',c)
+    c>0?console.log('not a prime'):console.log('prime number');
   }
 
-  let n=121;
+  let n=61;
   PrimeCheck(n)
