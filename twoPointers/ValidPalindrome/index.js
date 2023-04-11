cdbdce4a02933ae67e686a15d4648c4086593f2c
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-11 19:55:29 +0530

    check palindrome revised

--- twoPointers/ValidPalindrome/index.js
+++ twoPointers/ValidPalindrome/index.js
@@ -1,30 +1,28 @@
 
 
 function CheckPalindrome(str){
-     let obj={};
-     for(let i=0; i<str.length; i++){
+    let obj={};
+    for(let i=0; i<str.length; i++){
         if(obj[str[i]]===undefined){
             obj[str[i]]=1;
-        }
-        else{
+        }else{
             obj[str[i]]++;
         }
-     }
-    //  console.log(obj);
-    let c=0; 
+    }
+    let c=0;
     for(let key in obj){
-        if(obj[key]%2==1){
+        if(obj[key]%2===1){
             c++;
         }
     }
     if(c<3){
-       console.log('YES')
+        console.log('YES');
     }else{
-        console.log('NO')
+        console.log('NO');
     }
 }
 
 
 
-let str='abcbbbd';
+let str='abcbbb';
 CheckPalindrome(str);
