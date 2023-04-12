c3a093d7ec543a379786f8d50c78f49dfa898793
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-12 10:31:37 +0530

    duplicate from 2d array

--- TwoDArray/Duplicate_from_2D_Array/index.js
+++ TwoDArray/Duplicate_from_2D_Array/index.js
@@ -7,19 +7,18 @@ arr=[1,3,4,6,7];
 */
 
  function DuplicateRemove(arr){
-    let obj={};
-    for(let i=0; i<arr.length; i++){
-        let temp=arr[i];
-        for(let j=0; j<temp.length; j++){
-            if(obj[temp[j]]===undefined){
-                obj[temp[j]]=1;
-            }else{
-                obj[temp[j]]++;
-            }
-              
+   let obj={};
+   for(let i=0; i<arr.length; i++){
+    let temp=arr[i];
+     for(let j=0; j<temp.length; j++){
+        if(obj[temp[j]]===undefined){
+            obj[temp[j]]=1;
+        }else{
+            obj[temp[j]]++;
         }
-    }
-    console.log(Object.keys(obj))
+     }
+   }
+   console.log(Object.keys(obj));
  }
 
  DuplicateRemove([[1,3],[3,4],[4,6],[6,7]]);
