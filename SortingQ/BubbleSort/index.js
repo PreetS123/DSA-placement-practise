9765ed2f59b6c411c265b9fcaf514ef908b08194
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-12 11:17:24 +0530

    bubble sort done

--- SortingQ/BubbleSort/index.js
+++ SortingQ/BubbleSort/index.js
@@ -2,13 +2,13 @@
 
   function BubbleSort(arr,n){
      for(let i=0; i<n; i++){
-        for(let j=0; j<n-i+1; j++){
-            if(arr[j]>arr[j+1]){
-            let temp=arr[j];
-            arr[j]= arr[j+1];
-            arr[j+1]= temp;
-            }
-        }
+       for(let j=0; j<n-i+1; j++){
+         if(arr[j]>arr[j+1]){
+             let temp=arr[j];
+             arr[j]=arr[j+1];
+             arr[j+1]=temp;
+         }
+       }
      }
      return arr;
   }
