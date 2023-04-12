7bbcb385a3c811f5f28fd1d2bc5ef3ba223b7518
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-12 09:59:49 +0530

    making palindrome revised

--- twoPointers/makePalindrome/index.js
+++ twoPointers/makePalindrome/index.js
@@ -2,26 +2,24 @@
  const PalindromeSwap=(str)=>{
    let obj={};
    for(let i=0; i<str.length; i++){
-    if(obj[str[i]]===undefined){
+       if(obj[str[i]]===undefined){
         obj[str[i]]=1;
-    }else{
+       }
+       else{
         obj[str[i]]++;
-    }
+       }
    }
+
    let c=0;
    for(let key in obj){
-          if(obj[key]===1){
-            c++;
-          }
+    if(obj[key]%2==1){
+      c++;
+    }
    }
-   if(c<2){
-     console.log('isPalindrome')
-   }else{
-    console.log('notPalindrome')
-   }
+   c<2?console.log('YES'):console.log('NO');
 
  }
 
 
-let str='abkbke'
+let str='abkbkeaellmn'
  PalindromeSwap(str);
