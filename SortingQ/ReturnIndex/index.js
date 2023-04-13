e3abc205e179ccac63ca7ecb36a348bb4124d401
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-13 08:54:43 +0530

    return index

--- SortingQ/ReturnIndex/index.js
+++ SortingQ/ReturnIndex/index.js
@@ -4,31 +4,29 @@ order and print out the original indices of the new sorted array.
 Example:
 A=[4,5,3,7,1]
 After sorting the new array becomes A=[1,3,4,5,7].
-The required output should be "3 1 0 2 4";
+The required output should be "4 2 0 1 3";
 */
 
 
    function ReturnIndex(arr){
-        let n=arr.length;
+       let n= arr.length;
        let index=[];
        for(let i=0; i<n; i++){
-        index.push(i);
+          index.push(i);
        }
-       for(let i=0; i<n-1; i++){
-        for(let j=0; j<n-i-1; j++){
-            let temp,temp1;
-            if(arr[j]<arr[j+1]){
-                 temp=arr[j];
-                arr[j]= arr[j+1];
-                arr[j+1]=temp;
-
-                 temp1=index[j];
-                 index[j]= index[j+1];
-                  index[j+1]=temp1;
-            }
-        }
+       for(let i=0; i<n; i++){
+          for(let j=0; j<n-i+1; j++){
+               if(arr[j]>arr[j+1]){
+                    let t=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=t;
+                     let temp=index[j];
+                     index[j]= index[j+1];
+                     index[j+1]= temp;
+               }
+          }
        }
-        console.log(index.join(" "));
+        console.log(index.join(' '));
    }
 
 
