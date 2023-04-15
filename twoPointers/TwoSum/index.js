0cf5b456b11917310efb672d18baeb4d213cc99a
Preeti Sharma <97448477+PreetS123@users.noreply.github.com>
2023-04-15 11:01:04 +0530

    two sum

--- twoPointers/TwoSum/index.js
+++ twoPointers/TwoSum/index.js
@@ -1,25 +1,24 @@
 
 
   function TwoSum(n,k,arr){
-      let out='';
-      let s=0;
-      let e=n-1;
-      let flag=false;
-      while(s<=e){
-         if((arr[s]+arr[e])===k){
-            out=s+' '+e;
-            flag=true;
-            break;
-         }else if((arr[s]+arr[e])>k){
-             e--;
-             flag=false;
-         }
-         else{
-            s--;
-            flag=false;
-         }  
-      }
-      flag===true?console.log(out):console.log('-1 -1');
+    let s=0; 
+    let e=n-1;
+    let out;
+    let flag=false;
+    while(s<=e){
+       if((arr[s]+arr[e])==k){
+         out=s+' '+e;
+         flag=true;
+         break;
+       }else if((arr[s]+arr[e])>k){
+         e--;
+         flag=false;
+       }else{
+         s++;
+         flag=false;
+       }
+    }
+   flag==true?console.log(out):console.log("-1 -1");
   }
 
 
