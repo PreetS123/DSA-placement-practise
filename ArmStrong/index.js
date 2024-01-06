b5efcfe66f40bbf47fe05291283a2815cd990f15
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-06 17:09:33 +0530

    is an armstrong number

--- ArmStrong/index.js
+++ ArmStrong/index.js
@@ -1,13 +1,14 @@
   
    
       function ArmStrong(n){
-         let out=0;
-         n=n.toString();
-         let p=n.length;
-         for(let i=0; i<p; i++){
-            out+=n[i]**p;
-         }
-         out===parseInt(n)?console.log('Arm strong number'):console.log('Not an arm strong number');
+        let out=0;
+        n=n.toString();
+        let len=n.length;
+        for(let i=0; i<len; i++){
+           out+= (n[i]**len);
+        }
+        out==parseInt(n)?console.log("Is armstrong number"):console.log("Not an armstrong");
       }
-      let n=1634;
+      // let n=1634;
+      let n=153;
       ArmStrong(n)
