877ec63543cf902ab68d09b37f310bb3c9c071e0
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-06 16:31:31 +0530

    anagram completed

--- Anagram/index.js
+++ Anagram/index.js
@@ -1,27 +1,27 @@
 
 
 function AnaGramChk(x,y,n,m){
-    let flag=false;
+   let flag= false;
    if(n!==m){
-    flag=false;
+    return;
    }else{
-       x=x.split("").sort();
-       y=y.split("").sort();
-       for(let i=0; i<x.length; i++){
-          if(x[i]===y[i]){
-            flag=true;
-          }else{
-            fleg=false;
-            break;
-          }
+    x=x.split("").sort();
+    y=y.split("").sort();
+    for(let i=0; i<n; i++){
+       if(x[i]===y[i]){
+         flag=true;
+       }else{
+        flag=false;
+        break;
        }
+    }
    }
-   flag===true?console.log('Yes'):console.log('No');
+  flag===true?console.log("Is anagram"):console.log("Not a anagram");
 }
 
 
-let x='abcd';
-let y='ddbac';
+let x='aabcde';
+let y='abbcde';
 let n=x.length;
 let m=y.length;
 AnaGramChk(x,y,n,m);
