ce4522488fec3a990ca2a289935d9df03f81a2ec
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-06 17:21:50 +0530

    optimising the prime chk solution

--- PrimeChk/index.js
+++ PrimeChk/index.js
@@ -1,10 +1,10 @@
 function PrimeCheck(n) {
   let c = 0;
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i < n/2; i++) {
    if(n%i===0) c++;
   }
   c>0?console.log("Not a prime"):console.log("Is prime");
 }
 
-let n = 91;
+let n = 61;
 PrimeCheck(n);
