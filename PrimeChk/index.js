1b5e165c40766359ff9686f2ccfd100af269652f
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-06 17:27:00 +0530

    prime chk

--- PrimeChk/index.js
+++ PrimeChk/index.js
@@ -1,9 +1,9 @@
 function PrimeCheck(n) {
   let c = 0;
-  for (let i = 2; i < n/2; i++) {
-   if(n%i===0) c++;
+  for (let i = 2; i < n / 2; i++) {
+    if (n % i === 0) c++;
   }
-  c>0?console.log("Not a prime"):console.log("Is prime");
+  c > 0 ? console.log("Not a prime") : console.log("Is prime");
 }
 
 let n = 61;
