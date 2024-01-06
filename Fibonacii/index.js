36b46ec21afbc6460f5d1adb5fc7017446a73b8e
PreetS123 <preetbhardwaj517@gmail.com>
2024-01-06 17:51:30 +0530

    fibonacii number done

--- Fibonacii/index.js
+++ Fibonacii/index.js
@@ -1,17 +1,14 @@
-
-
-function FibonaciiSeries(n){
- let zero=0;
- let one=1;
- let sum=0;
- for(let i=1; i<n; i++){
-     sum=zero+one;
-     zero=one;
-     one=sum;
- }
- console.log(sum);
+function FibonaciiSeries(n) {
+  let f = 0;
+  let s = 1;
+  let sun = 0;
+  for (let i = 1; i < n; i++) {
+    sun = s + f;
+    f = s;
+    s = sun;
+  }
+  console.log(sun);
 }
 
-
-let n=9;
-FibonaciiSeries(n)
+let n = 9;
+FibonaciiSeries(n);
