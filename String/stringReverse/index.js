2faba825a9647adef3eeb130e0731730e73d6473
PreetS123 <preetbhardwaj517@gmail.com>
2024-02-01 16:41:19 +0530

    string reverse

--- String/stringReverse/index.js
+++ String/stringReverse/index.js
@@ -1,5 +1,7 @@
 /**
  * Reversing an array using inbuilt function.
+ * Time complexity is O(n);
+ * Space complexity is O(n), since their are n number of integers.
  */
 
 function reverseString(str){
@@ -7,3 +9,26 @@ function reverseString(str){
     console.log(strRev);
 }
 reverseString("Hello Love");
+
+// using For loop
+function reverseStr(str){
+    let rev="";
+    for(let i=str.length-1; i>=0; i--){
+        rev+=str[i];
+    }
+    console.log(rev);
+}
+
+reverseStr("Loveeeee");
+
+
+//Recursively
+function recursiveReverse(str){
+    if (str === "") {
+        return "";
+    } else {
+        return recursiveReverse(str.substr(1)) + str.charAt(0);
+    }
+}
+
+console.log(recursiveReverse("may"));
