28927e12b81291af409df1d2f86c9c93055727d3
PreetS123 <preetbhardwaj517@gmail.com>
2024-02-01 12:03:06 +0530

    bubble sort revised

--- SortingQ/BubbleSort/index.js
+++ SortingQ/BubbleSort/index.js
@@ -2,11 +2,11 @@
 
   function BubbleSort(arr,n){
     for(let i=0; i<n; i++){
-      for(let j=0; j<n-i+1; j++){
+      for(let j=0; j<n-i+1;j++){
         if(arr[j]>arr[j+1]){
-          let temp= arr[j];
-          arr[j]= arr[j+1];
-          arr[j+1]= temp;
+           let temp=arr[j];
+           arr[j]=arr[j+1];
+           arr[j+1]= temp;
         }
       }
     }
