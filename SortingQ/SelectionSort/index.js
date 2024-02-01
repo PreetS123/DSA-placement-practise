82df0714295a4b010b9343d65bce630a24058ab1
PreetS123 <preetbhardwaj517@gmail.com>
2024-02-01 13:28:43 +0530

    selection sort

--- SortingQ/SelectionSort/index.js
+++ SortingQ/SelectionSort/index.js
@@ -1,27 +1,23 @@
-
 /**
  * Selection Sort
  */
 
-function SelectionSort(arr,n){
-    for(let i=0; i<n-1; i++){
-        let min=i;
-       for(let j=i; j<n; j++){
-        if(arr[j]<arr[min]){
-           min=j;
-        }
-       }
-       let temp=arr[min]
-           arr[min]=arr[i];
-           arr[i]=temp;
+function SelectionSort(arr, n) {
+  for (let i = 0; i < n - 1; i++) {
+    let min = i;
+    for (let j = i; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
     }
-    return arr;
+    let temp = arr[min];
+    arr[min] = arr[i];
+    arr[i] = temp;
+  }
+  return arr;
 }
 
+let arr = [2, 4, 1, 3, 8, 6];
+let n = arr.length;
 
-
-
-let arr=[2,4,1,3,8,6];
-let n=arr.length;
-
-console.log(SelectionSort(arr,n));
+console.log(SelectionSort(arr, n));
