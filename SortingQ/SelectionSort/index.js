947b0e407f8504a6de98aef75652d77214add145
PreetS123 <preetbhardwaj517@gmail.com>
2024-02-01 13:27:24 +0530

    limit of array changed

--- SortingQ/SelectionSort/index.js
+++ SortingQ/SelectionSort/index.js
@@ -4,7 +4,7 @@
  */
 
 function SelectionSort(arr,n){
-    for(let i=0; i<=n-2; i++){
+    for(let i=0; i<n-1; i++){
         let min=i;
        for(let j=i; j<n; j++){
         if(arr[j]<arr[min]){
