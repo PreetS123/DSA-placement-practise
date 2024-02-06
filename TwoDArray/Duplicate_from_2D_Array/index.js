e25389709f45e81abb5aed18ee253332e7a39c78
PreetS123 <preetbhardwaj517@gmail.com>
2024-02-06 11:57:12 +0530

    remove dublicate using set and filter method

--- TwoDArray/Duplicate_from_2D_Array/index.js
+++ TwoDArray/Duplicate_from_2D_Array/index.js
@@ -7,18 +7,39 @@ arr=[1,3,4,6,7];
 */
 
  function DuplicateRemove(arr){
-   let obj={};
-   for(let i=0; i<arr.length; i++){
+  let obj={};
+  for(let i=0; i<arr.length; i++){
     let temp=arr[i];
-     for(let j=0; j<temp.length; j++){
-        if(obj[temp[j]]===undefined){
-            obj[temp[j]]=1;
-        }else{
-            obj[temp[j]]++;
-        }
-     }
-   }
-   console.log(Object.keys(obj));
+    for(let j=0; j<temp.length; j++){
+      if(obj[temp[j]]===undefined){
+         obj[temp[j]]=1;
+      }else{
+        obj[temp[j]]++;
+      }
+    }
+  }
+  console.log(Object.keys(obj));
  }
 
- DuplicateRemove([[1,3],[3,4],[4,6],[6,7]]);
+ let arr=[[1,3],[3,4],[4,6],[6,7]];
+//  DuplicateRemove(arr);
+
+// duplicate remove using flat and set method.
+ function DuplicateRemoveFlatSet(arr){
+  let flatArr= arr.flat();
+  // console.log(flatArr);
+   const uniqueEle= [...new Set(flatArr)];
+   console.log(uniqueEle);
+ }
+//  DuplicateRemoveFlatSet(arr);
+
+ // duplicate remove using filter method
+
+ function DuplicateRemoveFilter(arr){
+  let flatArr= arr.flat();
+  const uniqueArr=flatArr.filter((value,index,self)=>{
+    return self.indexOf(value)===index;
+  });
+  console.log(uniqueArr);
+ }
+ DuplicateRemoveFilter(arr);
